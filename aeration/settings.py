9208cdd8792c4ea9bf459bfaa946c4b9c6692ca1
web10-bariflo <web1.bariflo@outlook.com>
2025-11-19 10:55:10 +0530

    database changed

--- aeration/settings.py
+++ aeration/settings.py
@@ -78,16 +78,29 @@ WSGI_APPLICATION = 'aeration.wsgi.application'
 
 
 
+# DATABASES = {
+#     'default':{
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'aeration',                           
+#         'USER': 'Vertoxlabs',
+#         'PASSWORD':'Vtx@2025',
+#         'HOST': 'bcpostgressqlserver.postgres.database.azure.com',
+#         'PORT': '5432',
+#     }
+# }
+
+
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'aeration',                           
-        'USER': 'Vertoxlabs',
-        'PASSWORD':'Vtx@2025',
-        'HOST': 'bcpostgressqlserver.postgres.database.azure.com',
+        'USER': 'vertoxl',
+        'PASSWORD':'vertoxlabs',
+        'HOST': '192.168.1.140',
         'PORT': '5432',
     }
 }
+
 
 # DATABASES = {
 #     'default': {
